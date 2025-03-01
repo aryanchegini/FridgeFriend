844e24196e498fdb9bf73e0f595c4786628d8cef
Deniz Taskin <denizbleda@live.com>
2025-03-01 17:46:44 +0000

    modified auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,14 @@
+const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.JWT_SECRET || 'secret_key';
+
+
+/**
+ * Middleware function to authenticate requests using JWT.
+ * It verifies the token and extracts user data
+ */
+const authenticate = (req, res, next) => {
+}
+
 
 
 
