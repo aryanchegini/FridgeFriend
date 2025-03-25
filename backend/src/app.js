93125bacaa05fbb86df18b69134721be37ef84e0
Elijah <105212999+Elijah-AO@users.noreply.github.com>
2025-03-25 04:09:03 -0700

    added in app notification functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,6 +38,7 @@ app.use("/products", require("./routes/products.routes.js"));
 app.use("/groups", require("./routes/groups.routes.js"));
 app.use("/auth", require("./routes/auth.routes.js"));
 app.use("/barcodes", require("./routes/barcodes.routes"))
+app.use("/notifications", require("./routes/notifications.routes.js"));
 
 app.use(errorHandler);
 
